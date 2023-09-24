9378f6c035419e297149ebcc54ea1d02ce39e9bb
Walid Bouguima <bougwal@hilti.com>
2023-09-24 23:08:44 +0200

    feat:  -restructuring files  -correct location  -add css  -create respective design, home, orderForm thymeleaf templates

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -1,46 +1,43 @@
 package tacos;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
-
 import org.hibernate.validator.constraints.CreditCardNumber;
-
+import java.util.List;
+import java.util.ArrayList;
 import lombok.Data;
 
 @Data
 public class TacoOrder {
-    
-    @NotBlank(message="Delivery name is required")
-    private String deliveryName; 
 
-    @NotBlank(message="Street name is required")
-    private String deliveryStreet; 
+  @NotBlank(message="Delivery name is required")
+  private String deliveryName;
 
-    @NotBlank(message="city name is required")
-    private String deliveryCity; 
+  @NotBlank(message="Street is required")
+  private String deliveryStreet;
 
-    @NotBlank(message="State name is required")
-    private String deliveryState; 
+  @NotBlank(message="City is required")
+  private String deliveryCity;
 
-    @NotBlank(message="Zip name is required")
-    private String deliveryZip; 
+  @NotBlank(message="State is required")
+  private String deliveryState;
 
-    @CreditCardNumber(message="Credit Card Number is required")
-    private String ccNumber; 
+  @NotBlank(message="Zip code is required")
+  private String deliveryZip;
 
-    @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$", message="Must be formatted MM/YY")
-    private String ccExpiration; 
+  @CreditCardNumber(message="Not a valid credit card number")
+  private String ccNumber;
 
-    @Digits(integer =3, fraction = 0, message = "Invalid CVV")
-    private String ccCVV; 
+  @Pattern(regexp="^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$",
+           message="Must be formatted MM/YY")
+  private String ccExpiration;
 
-    private List<Taco> tacos = new ArrayList<>(); 
+  @Digits(integer=3, fraction=0, message="Invalid CVV")
+  private String ccCVV;
 
-    public void addTaco(Taco taco){
-        this.tacos.add(taco);
-    }
+  private List<Taco> tacos = new ArrayList<>();
+
+  public void addTaco(Taco taco) {
+    this.tacos.add(taco);
+  }
 }
