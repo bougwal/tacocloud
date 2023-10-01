7e2a6f03c002585073d96ff2efc54477f7c5cf53
Walid Bouguima <bougwal@hilti.com>
2023-10-01 15:20:53 +0200

    feat: consume OrderRepository in OrderController: save the order to the DB

--- src/main/java/tacos/web/OrderController.java
+++ src/main/java/tacos/web/OrderController.java
@@ -11,12 +11,20 @@ import org.springframework.web.bind.support.SessionStatus;
 
 import lombok.extern.slf4j.Slf4j;
 import tacos.TacoOrder;
+import tacos.data.OrderRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/orders")
 @SessionAttributes("tacoOrder")
 public class OrderController {
+
+  private OrderRepository orderRepository; 
+
+  public OrderController(OrderRepository orderRepo){
+    this.orderRepository = orderRepo; 
+  }
+
 
   @GetMapping("/current")
   public String orderForm() {
@@ -30,7 +38,7 @@ public class OrderController {
       return "orderForm";
     }
 
-    log.info("Order submitted: {}", order);
+    orderRepository.save(order);
     sessionStatus.setComplete();
 
     return "redirect:/";
