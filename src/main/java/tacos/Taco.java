b8aac8c1dcdc1b49c2c1ddbc698634cbaa7b74c0
Walid Bouguima <bougwal@hilti.com>
2023-10-08 16:19:40 +0200

    feat(fix):update Taco and TacoOrder annotations

--- src/main/java/tacos/Taco.java
+++ src/main/java/tacos/Taco.java
@@ -5,30 +5,21 @@ import java.util.List;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
-
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import tacos.web.IngredientRef;
-
 @Data
-@Table
-@EqualsAndHashCode(exclude = "createdAt")
 public class Taco {
 
-  @Id
-  private Long id; 
-  private Date createdAt = new Date(); 
   @NotNull
   @Size(min=5, message="Name must be at least 5 characters long")
   private String name;
 
+  private Date createdAt = new Date(); 
+
   @NotNull
   @Size(min=1, message="You must choose at least 1 ingredient")
-  private List<IngredientRef> ingredients = new ArrayList<>();
+  private List<Ingredient> ingredients = new ArrayList<>();
 
-  public void addIngredient(Ingredient taco){
-    this.ingredients.add(new IngredientRef(taco.getId()));
+  public void addIngredient(Ingredient ingredient){
+    this.ingredients.add(ingredient);
   }
 }
--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -4,7 +4,7 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 import org.hibernate.validator.constraints.CreditCardNumber;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.relational.core.mapping.Table;
+import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
 import java.io.Serializable;
@@ -14,13 +14,13 @@ import java.util.Date;
 import lombok.Data;
 
 @Data
-@Table
+@Document
 public class TacoOrder implements Serializable{
 
   private static final long serialVersionUID = 1L; 
   
   @Id
-  private Long id; 
+  private String id; 
   private Date placedAt = new Date(); 
   
   @NotBlank(message="Delivery name is required")
