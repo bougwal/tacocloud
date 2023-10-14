37b6cf70ffa1a9fec73d2fd5c97c07489b3bff89
Walid Bouguima <bougwal@hilti.com>
2023-10-14 20:11:28 +0200

    feat: update registration controller with @Controller annotation for component scanning

--- src/main/java/tacos/web/RegistrationController.java
+++ src/main/java/tacos/web/RegistrationController.java
@@ -1,6 +1,7 @@
 package tacos.web;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import tacos.data.UserRepository;
 import tacos.security.RegistrationForm;
 
+@Controller
 @RequestMapping("/register")
 public class RegistrationController {
     private UserRepository userRepository; 
