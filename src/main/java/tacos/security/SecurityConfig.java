cfffa15b5730a1cc6226103bda7d44f1584e0396
Walid Bouguima <bougwal@hilti.com>
2023-10-15 12:40:55 +0200

    feat: update security config to extend WebSecurityConfigAdapter

--- src/main/java/tacos/security/SecurityConfig.java
+++ src/main/java/tacos/security/SecurityConfig.java
@@ -1,24 +1,24 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 import tacos.User;
 import tacos.data.UserRepository;
 
 @Configuration
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter  {
     
-    @Bean
-    public PasswordEncoder passwordEncoder (){
-        return new BCryptPasswordEncoder(); 
-    }
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Bean
     public UserDetailsService userDetailsService(UserRepository userRepo){
@@ -31,26 +31,48 @@ public class SecurityConfig {
         };
     }
 
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        return http.authorizeRequests().mvcMatchers("/design", "/orders")
-        .hasRole("User")
-        .anyRequest()
-        .permitAll()
-        .and()
-        .formLogin()
-        .loginPage("/login")
-        .and()
-        .logout()
-        .logoutSuccessUrl("/")
-        .and()
-        .csrf()
-        .ignoringAntMatchers("/h2-console/**")
-        .and()
-        .headers()
-        .frameOptions()
-        .sameOrigin()
-        .and()
-        .build();
-    }
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+          .authorizeRequests()
+            .antMatchers("/design", "/orders").access("hasRole('USER')")
+            .antMatchers("/", "/**").access("permitAll")
+    
+          .and()
+            .formLogin()
+              .loginPage("/login")
+    
+          .and()
+            .logout()
+              .logoutSuccessUrl("/")
+    
+          // Make H2-Console non-secured; for debug purposes
+          .and()
+            .csrf()
+              .ignoringAntMatchers("/h2-console/**")
+    
+          // Allow pages to be loaded in frames from the same origin; needed for H2-Console
+          .and()
+            .headers()
+              .frameOptions()
+                .sameOrigin()
+          ;
+      }
+
+  @Bean
+  public PasswordEncoder encoder() {
+    return new BCryptPasswordEncoder();
+  }
+
+
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth)
+      throws Exception {
+
+    auth
+      .userDetailsService(userDetailsService)
+      .passwordEncoder(encoder());
+
+  }
+
 }
