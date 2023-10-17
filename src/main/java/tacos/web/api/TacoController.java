68d31ca0f18158235ab6a0318efaad365e418ab4
Walid Bouguima <bougwal@hilti.com>
2023-10-17 13:50:42 +0200

    feat: add fetching a taco by taco id rest endpoint

--- src/main/java/tacos/web/api/TacoController.java
+++ src/main/java/tacos/web/api/TacoController.java
@@ -1,9 +1,14 @@
 package tacos.web.api;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +31,9 @@ public class TacoController {
         PageRequest page = PageRequest.of(0, 12, Sort.by("createdAt").descending()); 
         return tacoRepository.findAll(page).getContent(); 
     }
-    
+
+    @GetMapping("/{id}")
+    public Optional<Taco> tacoById(@PathVariable("id") Long id){
+       return  tacoRepository.findById(id);
+    }
 }
